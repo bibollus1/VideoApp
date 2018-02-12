8e104b974a8b3a6540d3d93625350fb53ad56f8f
KBI <bibollus1@gmail.com>
2018-02-12 21:05:10 +0100

    Creating POST route for title and details

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 
 
 const port = 3002;
 const app = express(); // initialize app
 
+// Body parser
+app.use(bodyParser.json()); // converting user input into JSON
+app.use(bodyParser.urlencoded({extended: true}));
+
 // Connect to mongoose
 mongoose.connect('mongodb://localhost/vidapp-dev')
 .then(()=>console.log('MongoDB Connected...'))
 .catch(err=>console.log(err));
+
+//Load Idea Model
+require('./models/Idea');
+const Idea = mongoose.model('ideas');
+
 // handlebars middleware
 
 app.engine('handlebars', exphbs({
@@ -36,6 +46,46 @@ app.get('/', (req, res) => { // handling get request
 // about route
 app.get('/about', (req, res) => {
   res.render('about');
+});
+
+app.get('/ideas', (req, res) => {
+  res.render('about');
+});
+
+app.get('/ideas/add', (req, res) => {
+  res.render('ideas/add');
+});
+
+// Process form
+app.post('/ideas', (req, res)=>{
+  let errors = [];
+  if(!req.body.title){
+    errors.push({text:'Please add a title'});
+  }
+  if(!req.body.details){
+    errors.push({text:'Please add some details'});
+  }
+
+  if(errors.length>0){
+    res.render('ideas/add', {
+      errors: errors,
+      title: req.body.title,
+      details: req.body.details
+    });
+  } else {
+    const newUser = {
+      title: req.body.title,
+      details: req.body.details
+    //  user: req.user.id
+    }
+    new Idea(newUser)
+      .save()
+      .then(idea=>{
+        res.redirect('/ideas');
+      })
+  }
+  // console.log(req.body);
+  // res.send('Ok');
 });
 
 app.listen(port, () => {
