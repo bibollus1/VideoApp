eb8de868f0fac5950b125065f52b196cfee5470f
KBI <bibollus1@gmail.com>
2018-02-13 20:44:28 +0100

    Register form validation

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const passport = require('passport');
 const mongoose = require('mongoose');
 
 //Load Idea Model
@@ -15,4 +17,29 @@ router.get('/register', (req, res)=>{
   res.render('users/register');
 });
 
+// Register form POST
+router.post('/register', (req,res)=>{
+  let errors = [];
+
+  if (req.body.password != req.body.password2){
+    errors.push({text:'Password do not match'});
+  }
+
+  if (req.body.password.length<4){
+    errors.push({text:'Password must be at least 4 characters'})
+  }
+
+  if (errors.length>0){
+    res.render('users/register',{
+      // to not reenter form if fails, it will not clean web storage
+      errors: errors,
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+      password2: req.body.password2
+    });
+  } else {
+    res.send('Passed!');
+  }
+});
 module.exports = router;
